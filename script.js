2f3b86a000236a04b3a146948f727135fd2f51cf
jon <155998630+joyboyjon@users.noreply.github.com>
2026-04-21 12:33:57 +0300

    Create grid logic.

---
+++ script.js
@@ -0,0 +1,13 @@
+gridContainer=document.getElementById("grid-container");
+
+
+
+function addGridElements(){
+    for(let i=0;i<256;i++){
+        const gridElement=document.createElement("div");
+        gridElement.classList.add("grid-element");
+        gridContainer.appendChild(gridElement);
+    }
+}
+
+addGridElements();
